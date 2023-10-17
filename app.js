ef9b558f414bdce1b7e7a679829876acf7e1c674
Luiz Jurazek <luizjurazek@gmail.com>
2023-10-16 21:12:08 -0300

    Rotas de login

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const express = require('express')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-
 const app = express()
 
 // Database 
@@ -15,84 +12,18 @@ const {
 } = require('./middleware/auth.js')
 
 
-// Rotas
+// Importando rotas
 const transactionRoutes = require('./routes/transaction.js')
+const loginRoutes = require('./routes/login.js')
 
 // Middlewares
 app.use(express.json())
 
+
+// Rotas 
 app.use('/', transactionRoutes)
+app.use('/', loginRoutes)
 
-app.post('/cadastrar', async (req, res) => {
-    const username = req.body.username
-    const password = await bcrypt.hash(req.body.password, 8)
-    const email = req.body.email
-    const name = req.body.name
-
-    connection.query(`INSERT INTO users(s_user_users, s_password_users, s_email_users, s_nome_users) VALUES 
-    ("${username}", "${password}", "${email}", "${name}")`, (err) => {
-        if (err) {
-            console.error("Erro ao cadastrar o usuário: " + err)
-            return res.status(400).json({
-                error: true,
-                mensagem: "Erro ao cadastrar o usuário!"
-            })
-        } else {
-            return res.status(200).json({
-                error: false,
-                mensagem: "Usuário cadastrado com sucesso!"
-            })
-        }
-    })
-})
-
-app.post('/login', async (req, res) => {
-    const {
-        email,
-        password
-    } = req.body
-
-    try {
-        const [rows] = await connection.promise().query(`SELECT * FROM users WHERE s_email_users = ?`, [email])
-
-        // Verifica se o usuário foi encontrado
-        if (rows.length === 0) {
-            return res.status(400).json({
-                error: true,
-                mensagem: "Usuário não encontrado!"
-            })
-        }
-
-        // Verificando a senha do usuário
-        const user = rows[0]
-        if (!(await bcrypt.compare(password, user.s_password_users))) {
-            return res.status(400).json({
-                erro: true,
-                mensagem: "Erro usuário ou senha incorreto! Senha incorreta"
-            })
-        }
-
-        // const secretKey = gerarStringAleatoria(10)
-        const token = jwt.sign({
-            id: user.id_user_users
-        }, "CHAVE_SECRETA", {
-            expiresIn: '7d' // 7 dias
-        })
-
-        return res.json({
-            error: false,
-            message: 'Login realizado com sucesso!',
-            token,
-        })
-
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({
-            err: true,
-            mensagem: 'Erro interno do servidor',
-        });
-    }
-})
 
 
 app.listen(8080, () => {
