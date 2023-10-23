556dd94de523486a49fbd1a30bf925e616cbf54f
Luiz Jurazek <luizjurazek@gmail.com>
2023-10-23 19:47:08 -0300

    Modal logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,17 @@
-const logout = document.getElementById("logout")
+const btn_logout = document.getElementById("btn_logout")
+const popup_logout = document.getElementById("popup_logout")
+const btn_cancelar_logout = document.getElementById("btn_cancelar_logout")
 
-logout.addEventListener("click", (evt) => {
+popup_logout.addEventListener("click", (evt) => {
+    const modal_logout = document.getElementById("modal_logout")
+    modal_logout.classList.remove("ocultar")
+})
+
+btn_cancelar_logout.addEventListener("click", (evt) => {
+    modal_logout.classList.add("ocultar")
+})
+
+btn_logout.addEventListener("click", (evt) => {
     document.cookie = "token" + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     document.cookie = "user" + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     localStorage.removeItem("token");
