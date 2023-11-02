289c1853efe933ee7aeef0800b517effebe57437
Luiz Jurazek <luizjurazek@gmail.com>
2023-11-01 21:09:38 -0300

    refatoração nas rotas de categoria

--- routes/routesCategorias.js
+++ routes/routesCategorias.js
@@ -20,22 +20,31 @@ router.post('/insertcategoria', eAdmin, async (req, res) => {
     const categoriaExiste = todasCategorias.some((cat) => cat.s_categoria_config === categoria)
 
     if (categoriaExiste) {
-        return res.status(409).json({
-            error: `A categoria ${categoria} já existe.`
-        })
+        const response = {
+            erro: true,
+            mensagem: `A categoria ${categoria} já existe.`
+        }
+
+        return res.status(409).json(response)
     } else {
         try {
             const query = await connection.promise().query('INSERT INTO categorias (s_categoria_config) VALUES (?)', [categoria]);
             if (query[0].affectedRows > 0) {
-                return res.status(200).json({
+                const response = {
                     error: false,
+                    categoria: categoria,
                     mensagem: "Categoria criada com sucesso!"
-                })
+                }
+
+                return res.status(200).json(response)
             } else {
-                return res.status(400).json({
+                const response = {
                     error: true,
+                    categoria: categoria,
                     mensagem: "Houve um erro ao criar a categoria!"
-                })
+                }
+
+                return res.status(400).json(response)
             }
         } catch (erro) {
             console.error("Erro ao criar a categoria: " + erro)
@@ -49,17 +58,21 @@ router.post('/deletarcategoria/:categoria', eAdmin, async (req, res) => {
     try {
         let query = await connection.promise().query(`DELETE FROM categorias WHERE s_categoria_config = ?`, categoria)
         if (query[0].affectedRows > 0) {
-            return res.status(200).json({
+            const response = {
                 error: false,
                 categoria: categoria,
                 mensagem: `A categoria ${categoria} foi deletada com sucesso!`
-            })
+            }
+
+            return res.status(200).json(response)
         } else {
-            return res.status(400).json({
+            const response = {
                 error: true,
                 categoria: categoria,
                 mensagem: `Houve um erro ao deletar a categoria ${categoria}.`
-            })
+            }
+            
+            return res.status(400).json(response)
         }
     } catch (error) {
         console.error("Erro ao deletar a categoria: " + error)
@@ -73,17 +86,21 @@ router.put('/editarcategoria', eAdmin, async (req, res) => {
     try {
         let query = await connection.promise().query(`UPDATE categorias SET s_categoria_config = '${novaCategoria}' WHERE s_categoria_config = ?`, categoria)
         if (query[0].affectedRows > 0) {
-            return res.status(200).json({
+            const response = {
                 error: false,
                 categoria: categoria,
                 mensagem: `Categoria editada com sucesso!`
-            })
+            }
+
+            return res.status(200).json(response)
         } else {
-            return res.status(400).json({
+            const response = {
                 error: true,
                 categoria: categoria,
                 mensagem: `Houve um erro ao editar a categoria ${categoria}.`
-            })
+            }
+
+            return res.status(400).json(response)
         }
     } catch (error) {
         console.error("Erro ao deletar a categoria: " + error)
